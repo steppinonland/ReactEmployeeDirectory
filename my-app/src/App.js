43a27c43ff4e8276155f7d91a556e817cdb24fbb
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-09-01 21:18:40 -0500

    my react app and testing groundwork

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+// need to put in all of the app here
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      </header>
+    </div>
+  );
+}
+
+export default App;
