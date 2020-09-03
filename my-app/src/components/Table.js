4eb6b4178e9f33d7bb69fc2639b1a9a17f244f33
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-09-02 19:40:26 -0500

    put in the seed API link and imported table. chose a table from react bootstrap

--- my-app/src/components/Table.js
+++ my-app/src/components/Table.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import Table from 'react-bootstrap/Table'
-
+// https://randomuser.me/api/?results=200&nat=us
 class Table extends Component {
     constructor()
     super();
     state = {
         columns: {
-            name: "name",
-            id: "id",
+            first_name: "first name",
+            last_name: "last name",
+            email: "email",
             phone: "phone"
         }
+    }
+    render() {
+        return(
+            <Table />
+        );
     }
 }
 
